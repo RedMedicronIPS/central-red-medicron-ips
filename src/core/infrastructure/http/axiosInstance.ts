770bb972204ca7468d62fcd9a53844d4b40aa69a
Edisonnarvaez <edison.narvaez@campusucc.edu.co>
2025-07-31 12:28:55 -0500

    se ajusta para servidor

--- src/core/infrastructure/http/axiosInstance.ts
+++ src/core/infrastructure/http/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://152.200.161.234:8081/api',
   headers: {
     'Content-Type': 'application/json',
   },
