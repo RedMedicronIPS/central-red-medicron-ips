89343818ba6665bf1aa85063428f2108a6d00bae
RedMedicronIPS <coordinaciontigi@redmedicronips.com.co>
2025-09-15 13:08:23 -0500

    ajustes rutas

--- src/core/infrastructure/http/axiosInstance.ts
+++ src/core/infrastructure/http/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://152.200.161.234:8081/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },
